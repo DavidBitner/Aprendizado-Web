f7f1af02dd00f60204c154f9ae023072731bcc3d
David <david-bitner@hotmail.com>
2020-11-09 06:37:48 -0300

    Create aula03-hello-world.php

---
+++ curso2-php-basico/aula03-hello-world.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello World</title>
+</head>
+<body>
+    <h1>Testando PHP</h1>
+    <?php
+        echo '<h2>Hello, World!</h2>';
+    ?>
+    <?= 'Utilizando a tag de impressão' ?>
+</body>
+</html>
