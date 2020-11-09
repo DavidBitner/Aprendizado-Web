ccf396bbc228b1897c7f95e2418cb3d2db3d601e
David <david-bitner@hotmail.com>
2020-11-09 07:23:48 -0300

    Create aula04-variaveis.php

---
+++ curso2-php-basico/aula04-variaveis.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Variáveis</title>
+</head>
+
+<body>
+    <?php 
+        $numero = 4;
+        $nome = 'Doidera';
+        echo "$nome tem $numero anos";
+    ?>
+</body>
+
+</html>
