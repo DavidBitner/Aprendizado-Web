52389998af65509c0aa58109f647bfe0113625c0
David <david-bitner@hotmail.com>
2021-01-06 13:25:18 -0300

    Create lecture4-basic-operators.js

---
+++ curso5-javascript-int-adv/module1-js-fundamentals1/lecture4-basic-operators.js
@@ -0,0 +1,10 @@
+let country = 'Brasil'
+const continent = 'South America'
+let population = 200
+let half_population = population / 2
+const isIsland = false
+let language = 'Portuguese'
+
+console.log(half_population + ' million people is half the population of Brazil')
+console.log(`The average population of a country is 33 million people. ${country} have more people than the average country`)
+console.log(`${country} is in ${continent}, and its ${population} million people speak ${language}`)
