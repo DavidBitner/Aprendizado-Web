278c557d90295b388b4d725ff8208427db950d39
David <david-bitner@hotmail.com>
2021-02-24 21:59:37 -0300

    Create l8-type-conversion-coercion.js

---
+++ curso5-javascript-int-adv/module1-js-fundamentals1/l8-type-conversion-coercion.js
@@ -0,0 +1,18 @@
+// type conversion
+console.log("Type Conversion")
+const input_year = '1991'
+console.log(Number(input_year) + input_year)
+console.log(Number(input_year) + 18)
+
+console.log(Number('Jonas'))
+
+console.log(String(23), 23)
+
+// type coercion
+console.log("Type Coercion")
+console.log(`I'm ${Number(23)} years old`)
+console.log('9' - '5')
+console.log('19' - '13' + '17')
+console.log('19' - '13' + 17)
+console.log('123' < 57)
+console.log(5 + 6 + '4' + 9 - 4 - 2)
