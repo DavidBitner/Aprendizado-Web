883bc8afcf9df0ca6fc2913623bdaeeb84ff05d2
David <david-bitner@hotmail.com>
2021-02-25 20:52:21 -0300

    Create l9-truthy-falsy-values.js

---
+++ curso5-javascript-int-adv/module1-js-fundamentals1/l9-truthy-falsy-values.js
@@ -0,0 +1,19 @@
+/* Falsy value é quando um valor, quando convertido para boolean, será dado como falso, são eles:
+0, '', undefined, null, NaN
+
+Truthy value é quando um valor, que não os falsy é dado como verdadeiro
+*/
+
+console.log(Boolean(0))
+console.log(Boolean(''))
+console.log(Boolean(undefined))
+console.log(Boolean(null))
+console.log(Boolean(NaN))
+
+// Quando um valor qualquer for colocado dentro de um if/else statement, esse numero será convertido automáticamente para boolean
+const money = 0
+if (money) {
+  console.log('You have money!')
+} else {
+  console.log('You have no money!')
+}
