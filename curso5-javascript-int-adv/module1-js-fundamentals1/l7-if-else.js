84df6ebd45c06078c246ecea6d6d4f7d67a1d612
David <david-bitner@hotmail.com>
2021-02-24 21:28:07 -0300

    Create l7-if-else.js

---
+++ curso5-javascript-int-adv/module1-js-fundamentals1/l7-if-else.js
@@ -0,0 +1,19 @@
+const age = 19
+const is_old_enough = age >= 18
+
+if (is_old_enough) {
+  console.log(`The user is old enough to drive`)
+} else {
+  const years_left = 18 - age
+  console.log(`The user isn't old enough to drive. Wait ${years_left} years.`)
+}
+
+const birth_year = 1998
+
+let century
+if (birth_year <= 2000) {
+  century = 20
+} else {
+  century = 21
+}
+console.log(century)
