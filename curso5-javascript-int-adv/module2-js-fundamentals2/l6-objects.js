c20d2628949fd0af223cf7098941bf9823e807d8
David <david-bitner@hotmail.com>
2021-03-03 05:12:45 -0300

    Create l6-objects.js

---
+++ curso5-javascript-int-adv/module2-js-fundamentals2/l6-objects.js
@@ -0,0 +1,33 @@
+// Array
+const david_array = [
+  'David',
+  'Bitner',
+  2021 - 1996,
+  'Programador',
+  ['James', 'John', 'Johnson']
+]
+
+// Object
+const david_object = {
+  first_name: 'David',
+  last_name: 'Bitner',
+  age: 2021 - 1996,
+  ocupation: 'Programador',
+  friends: ['James', 'John', 'Johnson']
+}
+
+/**
+ * Atividade
+ * 
+ * 1.Create an object called 'myCountry'for a country of your choice, containing properties 'country', 'capital', 'language', 'population'and 'neighbours'(an array like we used in previous assignments)
+*/
+
+const my_country = {
+  country: 'Brasil',
+  capital: 'Brasilia',
+  language: 'Português',
+  population: 210,
+  neighbours: ['Argentina', 'Peru', 'Uruguai', 'Venezuela']
+}
+
+console.log(my_country)
