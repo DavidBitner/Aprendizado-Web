518090fe848197c1e57a49ac692b1f20c3cb1cf4
David <david-bitner@hotmail.com>
2021-03-09 11:20:24 -0300

    Update script.js
    Again Button

--- curso5-javascript-int-adv/module4-dom-events/p1-guess-my-number/script.js
+++ curso5-javascript-int-adv/module4-dom-events/p1-guess-my-number/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const correct_number = Math.trunc(Math.random() * 20) + 1;
+let correct_number = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
 let high_score = 0;
@@ -8,11 +8,11 @@ let high_score = 0;
 document.querySelector(`.check`).addEventListener("click", function () {
   const guess = document.querySelector(`.guess`).value;
 
-  // Quando não há input
+  // No input
   if (!guess) {
     document.querySelector(`.message`).textContent = "Invalid Number!";
 
-    // Quando o jogador ganha
+    // When the player wins
   } else if (guess == correct_number) {
     document.querySelector(`.message`).textContent = "Correct Number!";
     if (high_score < score) {
@@ -22,26 +22,26 @@ document.querySelector(`.check`).addEventListener("click", function () {
     document.querySelector(`.number`).style.width = "30rem";
     document.querySelector(`.number`).textContent = correct_number;
 
-    // Quando o palpite é muito alto
+    // When the guess is too high
   } else if (guess > correct_number) {
     if (score > 1) {
       document.querySelector(`.message`).textContent = "Too High!";
       score--;
 
-      // Quando o jogador perde
+      // When the player loses
     } else {
       document.querySelector(`.message`).textContent = "You Lost!";
       score = 0;
       document.querySelector(`body`).style.backgroundColor = "red";
     }
 
-    // Quando o palpite é muito baixo
+    // When the guess is too low
   } else {
     if (score > 1) {
       document.querySelector(`.message`).textContent = "Too Low!";
       score--;
 
-      // Quando o jogador perde
+      // When the player loses
     } else {
       document.querySelector(`.message`).textContent = "You Lost!!";
       score = 0;
@@ -50,4 +50,14 @@ document.querySelector(`.check`).addEventListener("click", function () {
   }
   document.querySelector(`.score`).textContent = score;
   document.querySelector(`.highscore`).textContent = high_score;
+});
+
+// Restart game
+document.querySelector(`.again`).addEventListener("click", function () {
+  score = 20;
+  correct_number = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector(`body`).style.backgroundColor = "#222";
+  document.querySelector(`.number`).style.width = "15rem";
+  document.querySelector(`.number`).textContent = "?";
+  document.querySelector(`.guess`).value = "";
 });
