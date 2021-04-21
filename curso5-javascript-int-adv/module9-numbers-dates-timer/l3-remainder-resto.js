3fa42c586a5f4f67d13c6d3cbc36c6206229152b
David <david-bitner@hotmail.com>
2021-04-21 18:28:48 -0300

    Create l3-remainder-resto.js

---
+++ curso5-javascript-int-adv/module9-numbers-dates-timer/l3-remainder-resto.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// Resto
+console.log(5 % 2);
+console.log(5 / 2); // 5 = 2 * 2 + 1
+
+console.log(8 % 3);
+console.log(9 % 3);
+
+// Par e Impar
+const is_even = (n) => n % 2 === 0;
+const is_odd = (n) => n % 2 !== 0;
+
+console.log(is_even(4));
+console.log(is_odd(4));
