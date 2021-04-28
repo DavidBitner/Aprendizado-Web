b46a471baaf162fc35e36dd20db7e31e051000e6
David <david-bitner@hotmail.com>
2021-04-28 09:17:47 -0300

    Create l4-smooth-scrolling.js

---
+++ curso5-javascript-int-adv/module10-advanced-dom/l4-smooth-scrolling.js
@@ -0,0 +1,16 @@
+'use strict'
+
+// JavaScript
+const btn_scroll_to = document.querySelector(".btn-scroll-to");
+const section_1 = document.querySelector("#section-1");
+
+btn_scroll_to.addEventListener("click", function () {
+  section_1.scrollIntoView({ behavior: "smooth" });
+});
+
+// CSS
+/* 
+html {
+  scroll-behavior: smooth;
+} 
+*/
