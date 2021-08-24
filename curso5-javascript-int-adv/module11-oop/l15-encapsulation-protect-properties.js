0f5b0827fe0faca8f46110a0d2e26e2cce44bcc4
David <david-bitner@hotmail.com>
2021-08-24 18:19:27 -0300

    Update l15-encapsulation-protect-properties.js

--- curso5-javascript-int-adv/module11-oop/l15-encapsulation-protect-properties.js
+++ curso5-javascript-int-adv/module11-oop/l15-encapsulation-protect-properties.js
@@ -1,13 +1,13 @@
 "use strict";
 
-// Até o momento dessa aula, ainda não é possível ter uma propriedade realmente privada dentro de JS, por isso é comum que seja usado um "_" na frente de toda propriedade para apontar que aquela propriedade é protegida e deveria ser privada
+// É comum que seja usado um "_" na frente de toda propriedade para apontar que aquela propriedade é protegida e deveria ser privada
 class Account {
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
-    this.pin = pin;
-
+    
     // Propriedade protegida
+    this._pin = pin;
     this._movements = [];
   }
 
