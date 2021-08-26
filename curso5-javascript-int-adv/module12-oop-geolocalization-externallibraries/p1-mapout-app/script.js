d5e08591ba507c1fab905fe9ec682239ea5514e4
David <david-bitner@hotmail.com>
2021-08-26 19:44:24 -0300

    Update script.js
    Função para colocar popups ao clicar no mapa

--- curso5-javascript-int-adv/module12-oop-geolocalization-externallibraries/p1-mapout-app/script.js
+++ curso5-javascript-int-adv/module12-oop-geolocalization-externallibraries/p1-mapout-app/script.js
@@ -19,14 +19,16 @@ year.innerHTML = today.getFullYear();
 // Função para conseguir a localização do usuário
 navigator.geolocation.getCurrentPosition(
   function (position) {
+    // Definindo coordenadas atuais do usuário
     const { latitude } = position.coords;
     const { longitude } = position.coords;
     const coords = [latitude, longitude];
 
-    // Leaflet
+    // Variáveis usadas no mapa
     const map = L.map("map").setView(coords, 13);
-    const marker = L.marker(coords).addTo(map);
+    const marker_posicao_usuario = L.marker(coords).addTo(map);
 
+    // Função de loadind do mapa
     L.tileLayer(
       "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
       {
@@ -40,6 +42,29 @@ navigator.geolocation.getCurrentPosition(
           "pk.eyJ1IjoiZGF2aWQzODIxIiwiYSI6ImNrc3RmbnlkNDE2Mmsyd2xtbXA0MWpuc3kifQ.RbxAc34mJzDrGhZZT7e2Sg",
       }
     ).addTo(map);
+
+    // Função para colocar um popup ao clicar em uma posição do mapa
+    map.on("click", function (map_event) {
+      // Desconstruindo em variáveis o objeto "latlng" que se encontra dentro do objeto "map_event"
+      const { lat, lng } = map_event.latlng;
+
+      // Popup
+      L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(
+          // Definindo configurações do popup
+          L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: "running-popup",
+          })
+        )
+        // Definindo texto dentro do popup
+        .setPopupContent(`Workout ${lat}:${lng}`)
+        .openPopup();
+    });
   },
   function () {
     alert("Could not get your position.");
