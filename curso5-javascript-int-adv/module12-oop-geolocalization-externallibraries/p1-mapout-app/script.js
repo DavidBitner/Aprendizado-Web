e1c75353e0c9d08ca509bfc4ff8ffb0aef25f136
David <david-bitner@hotmail.com>
2021-08-27 21:51:10 -0300

    Update script.js
    Funções de mostrar formulário e alterar formulário baseado no tipo de exercicio selecionado

--- curso5-javascript-int-adv/module12-oop-geolocalization-externallibraries/p1-mapout-app/script.js
+++ curso5-javascript-int-adv/module12-oop-geolocalization-externallibraries/p1-mapout-app/script.js
@@ -1,7 +1,6 @@
 "use strict";
 
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
+// Variáveis
 const form = document.querySelector(".form");
 const container_workouts = document.querySelector(".workouts");
 const input_type = document.querySelector(".form-input-type");
@@ -10,8 +9,25 @@ const input_duration = document.querySelector(".form-input-duration");
 const input_cadence = document.querySelector(".form-input-cadence");
 const input_elevation = document.querySelector(".form-input-elevation");
 
+let map_event, map;
+
 const year = document.querySelector("#year");
 const today = new Date();
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
 // Copyright year
 year.innerHTML = today.getFullYear();
@@ -25,7 +41,7 @@ navigator.geolocation.getCurrentPosition(
     const coords = [latitude, longitude];
 
     // Variáveis usadas no mapa
-    const map = L.map("map").setView(coords, 13);
+    map = L.map("map").setView(coords, 13);
     const marker_posicao_usuario = L.marker(coords).addTo(map);
 
     // Função de loadind do mapa
@@ -43,30 +59,56 @@ navigator.geolocation.getCurrentPosition(
       }
     ).addTo(map);
 
-    // Função para colocar um popup ao clicar em uma posição do mapa
-    map.on("click", function (map_event) {
-      // Desconstruindo em variáveis o objeto "latlng" que se encontra dentro do objeto "map_event"
-      const { lat, lng } = map_event.latlng;
+    // Função executada ao clicar no mapa
+    map.on("click", function (map_e) {
+      map_event = map_e;
 
-      // Popup
-      L.marker([lat, lng])
-        .addTo(map)
-        .bindPopup(
-          // Definindo configurações do popup
-          L.popup({
-            maxWidth: 250,
-            minWidth: 100,
-            autoClose: false,
-            closeOnClick: false,
-            className: "running-popup",
-          })
-        )
-        // Definindo texto dentro do popup
-        .setPopupContent(`Workout ${lat}:${lng}`)
-        .openPopup();
+      // Abrindo formulário de exercicio ao clicar no mapa
+      form.classList.remove("hidden");
+      input_distance.focus();
     });
   },
   function () {
     alert("Could not get your position.");
   }
 );
+
+form.addEventListener("submit", function (e) {
+  // Prevenir que a página recarregue ao enviar formulário
+  e.preventDefault();
+
+  // Limpando campos do formulário depois de envia-lo
+  input_cadence.value =
+    input_distance.value =
+    input_duration.value =
+    input_elevation.value =
+      "";
+
+  // Desconstruindo em variáveis o objeto "latlng" que se encontra dentro do objeto "map_event"
+  const { lat, lng } = map_event.latlng;
+
+  // Marker e popup adicionados ao enviar formulário
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      // Definindo configurações do popup
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: "running-popup",
+      })
+    )
+    // Definindo texto dentro do popup
+    .setPopupContent(`Workout ${lat}:${lng}`)
+    .openPopup();
+});
+
+// Event listener para quando o tipo de exercicio for selecionado
+input_type.addEventListener("change", function () {
+  // Seleção dos dois campos que serão alterados quando for alterado o tipo de exercicio
+  // Usando método "closest" para selecionar o parent mais próximo
+  input_elevation.closest(".form-row").classList.toggle("form-row-hidden");
+  input_cadence.closest(".form-row").classList.toggle("form-row-hidden");
+});
