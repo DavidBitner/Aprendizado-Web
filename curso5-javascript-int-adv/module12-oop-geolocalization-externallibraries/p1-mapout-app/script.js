bb85ed9b0eb6b13a8fa6e09f7a1598f4b5155282
David <david-bitner@hotmail.com>
2021-08-27 22:48:34 -0300

    Update script.js
    Código reestruturado em classes

--- curso5-javascript-int-adv/module12-oop-geolocalization-externallibraries/p1-mapout-app/script.js
+++ curso5-javascript-int-adv/module12-oop-geolocalization-externallibraries/p1-mapout-app/script.js
@@ -29,20 +29,39 @@ const months = [
   "December",
 ];
 
-// Copyright year
-year.innerHTML = today.getFullYear();
+class App {
+  // Variáveis privadas da classe
+  #map_event;
+  #map;
 
-// Função para conseguir a localização do usuário
-navigator.geolocation.getCurrentPosition(
-  function (position) {
+  constructor() {
+    this._get_position();
+    form.addEventListener("submit", this._new_workout.bind(this));
+
+    // Event listener para quando o tipo de exercicio for selecionado
+    input_type.addEventListener("change", this._toggle_elevation_field);
+  }
+
+  // Função para conseguir a localização do usuário
+  _get_position() {
+    navigator.geolocation.getCurrentPosition(
+      this._load_map.bind(this),
+      function () {
+        alert("Could not get your position.");
+      }
+    );
+  }
+
+  // Função de carregamento do mapa
+  _load_map(position) {
     // Definindo coordenadas atuais do usuário
     const { latitude } = position.coords;
     const { longitude } = position.coords;
     const coords = [latitude, longitude];
 
     // Variáveis usadas no mapa
-    map = L.map("map").setView(coords, 13);
-    const marker_posicao_usuario = L.marker(coords).addTo(map);
+    this.#map = L.map("map").setView(coords, 13);
+    const marker_posicao_usuario = L.marker(coords).addTo(this.#map);
 
     // Função de loadind do mapa
     L.tileLayer(
@@ -57,58 +76,61 @@ navigator.geolocation.getCurrentPosition(
         accessToken:
           "pk.eyJ1IjoiZGF2aWQzODIxIiwiYSI6ImNrc3RmbnlkNDE2Mmsyd2xtbXA0MWpuc3kifQ.RbxAc34mJzDrGhZZT7e2Sg",
       }
-    ).addTo(map);
+    ).addTo(this.#map);
 
     // Função executada ao clicar no mapa
-    map.on("click", function (map_e) {
-      map_event = map_e;
-
-      // Abrindo formulário de exercicio ao clicar no mapa
-      form.classList.remove("hidden");
-      input_distance.focus();
-    });
-  },
-  function () {
-    alert("Could not get your position.");
+    this.#map.on("click", this._show_form.bind(this));
   }
-);
 
-form.addEventListener("submit", function (e) {
-  // Prevenir que a página recarregue ao enviar formulário
-  e.preventDefault();
+  _show_form(map_e) {
+    this.#map_event = map_e;
 
-  // Limpando campos do formulário depois de envia-lo
-  input_cadence.value =
-    input_distance.value =
-    input_duration.value =
-    input_elevation.value =
-      "";
+    // Abrindo formulário de exercicio ao clicar no mapa
+    form.classList.remove("hidden");
+    input_distance.focus();
+  }
 
-  // Desconstruindo em variáveis o objeto "latlng" que se encontra dentro do objeto "map_event"
-  const { lat, lng } = map_event.latlng;
+  _toggle_elevation_field() {
+    // Seleção dos dois campos que serão alterados quando for alterado o tipo de exercicio
+    // Usando método "closest" para selecionar o parent mais próximo
+    input_elevation.closest(".form-row").classList.toggle("form-row-hidden");
+    input_cadence.closest(".form-row").classList.toggle("form-row-hidden");
+  }
 
-  // Marker e popup adicionados ao enviar formulário
-  L.marker([lat, lng])
-    .addTo(map)
-    .bindPopup(
-      // Definindo configurações do popup
-      L.popup({
-        maxWidth: 250,
-        minWidth: 100,
-        autoClose: false,
-        closeOnClick: false,
-        className: "running-popup",
-      })
-    )
-    // Definindo texto dentro do popup
-    .setPopupContent(`Workout ${lat}:${lng}`)
-    .openPopup();
-});
+  _new_workout(e) {
+    // Prevenir que a página recarregue ao enviar formulário
+    e.preventDefault();
 
-// Event listener para quando o tipo de exercicio for selecionado
-input_type.addEventListener("change", function () {
-  // Seleção dos dois campos que serão alterados quando for alterado o tipo de exercicio
-  // Usando método "closest" para selecionar o parent mais próximo
-  input_elevation.closest(".form-row").classList.toggle("form-row-hidden");
-  input_cadence.closest(".form-row").classList.toggle("form-row-hidden");
-});
+    // Limpando campos do formulário depois de envia-lo
+    input_cadence.value =
+      input_distance.value =
+      input_duration.value =
+      input_elevation.value =
+        "";
+
+    // Desconstruindo em variáveis o objeto "latlng" que se encontra dentro do objeto "map_event"
+    const { lat, lng } = this.#map_event.latlng;
+
+    // Marker e popup adicionados ao enviar formulário
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        // Definindo configurações do popup
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: "running-popup",
+        })
+      )
+      // Definindo texto dentro do popup
+      .setPopupContent(`Workout ${lat}:${lng}`)
+      .openPopup();
+  }
+}
+
+const app = new App();
+
+// Copyright year
+year.innerHTML = today.getFullYear();
