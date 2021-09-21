8e17809755e84300255f67e428a5b7d7690b8a90
David <david-bitner@hotmail.com>
2021-09-21 17:52:38 -0300

    Update recipe-app
    Refactored code to fit MVC architecture

---
+++ curso5-javascript-int-adv/module15-recipe-app/recipe-app/src/js/model.js
@@ -0,0 +1,30 @@
+export let state = {
+  recipe: {},
+};
+
+export async function loadRecipe(recipeId) {
+  try {
+    const response = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${recipeId}`
+    );
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(`${data.message} (${response.status})`);
+    }
+
+    const { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+  } catch (error) {
+    alert(error);
+  }
+}
