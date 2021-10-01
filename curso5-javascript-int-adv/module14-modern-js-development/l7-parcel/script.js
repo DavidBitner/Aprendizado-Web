ce762e56555233f7323c574e93020f8225ed4459
David <david-bitner@hotmail.com>
2021-10-01 14:38:38 -0300

    Update script.js

--- curso5-javascript-int-adv/module14-modern-js-development/l7-parcel/script.js
+++ curso5-javascript-int-adv/module14-modern-js-development/l7-parcel/script.js
@@ -8,6 +8,7 @@ npm i parcel --save-dev = Instalar parcel
 npx parcel nomeDoHtml.html = Fazer o bundle da aplicação
 npm run start = Fazer o bundle da aplicação depois de criar o script dentro de package.json
 npm run build = Fazer o bundle final depois de criar o script dentro de package.json
+IMPORTANTE: Antes de fazer o bundle final, deletar as pastas ".parcel-cache" e "dist"
 
 Depois de criar o script "start": "parcel index.html" dentro do arquivo "package.json", é possível iniciar o servidor pelo parcel com o comando npm run start
 "build": "parcel build index.html" dentro do arquivo "package.json" serve para montar o pacote final da aplicação
@@ -16,7 +17,6 @@ Depois de criar o script "start": "parcel index.html" dentro do arquivo "package
 import cloneDeep from "./node_modules/lodash-es/cloneDeep.js";
 import { addToCart, cart } from "./shoppingCart.js";
 // import cloneDeep from "lodash-es";
-
 
 addToCart("pizza", 5);
 addToCart("oranges", 12);
